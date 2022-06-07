e3c43a6b958af007950efee9a16117c4c9e1d48b
chandresh33 <55847367+chandresh33@users.noreply.github.com>
2022-06-07 12:38:15 +0100

    Add files via upload

---
+++ final_year_project.o.d
@@ -0,0 +1,46 @@
+final_year_project.o: ../final_year_project.c \
+  c:/programs/winavr/lib/gcc/../../avr/include/avr/io.h \
+  c:/programs/winavr/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/programs/winavr/lib/gcc/../../avr/include/inttypes.h \
+  c:/programs/winavr/lib/gcc/../../avr/include/stdint.h \
+  c:/programs/winavr/lib/gcc/../../avr/include/avr/iom328p.h \
+  c:/programs/winavr/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/programs/winavr/lib/gcc/../../avr/include/avr/common.h \
+  c:/programs/winavr/lib/gcc/../../avr/include/avr/version.h \
+  c:/programs/winavr/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/programs/winavr/lib/gcc/../../avr/include/avr/lock.h \
+  c:/programs/winavr/lib/gcc/../../avr/include/util/delay.h \
+  c:/programs/winavr/lib/gcc/../../avr/include/util/delay_basic.h \
+  c:/programs/winavr/lib/gcc/../../avr/include/stdio.h \
+  c:\programs\winavr\bin\../lib/gcc/avr/4.3.3/include/stdarg.h \
+  c:\programs\winavr\bin\../lib/gcc/avr/4.3.3/include/stddef.h
+
+c:/programs/winavr/lib/gcc/../../avr/include/avr/io.h:
+
+c:/programs/winavr/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/programs/winavr/lib/gcc/../../avr/include/inttypes.h:
+
+c:/programs/winavr/lib/gcc/../../avr/include/stdint.h:
+
+c:/programs/winavr/lib/gcc/../../avr/include/avr/iom328p.h:
+
+c:/programs/winavr/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/programs/winavr/lib/gcc/../../avr/include/avr/common.h:
+
+c:/programs/winavr/lib/gcc/../../avr/include/avr/version.h:
+
+c:/programs/winavr/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/programs/winavr/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/programs/winavr/lib/gcc/../../avr/include/util/delay.h:
+
+c:/programs/winavr/lib/gcc/../../avr/include/util/delay_basic.h:
+
+c:/programs/winavr/lib/gcc/../../avr/include/stdio.h:
+
+c:\programs\winavr\bin\../lib/gcc/avr/4.3.3/include/stdarg.h:
+
+c:\programs\winavr\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
